5e1a420bc7f8c5df621888b384676dc656bcc5f7
geedtd <grcazares@gmail.com>
2023-05-04 20:30:46 -0700

    working on authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ export const register = async( req, res) => {
     }
 };
 
-
+//LOGIN AUTHENTICATION
 export const login = async ( req, res) => {
     try {
         const { email, password } = req.body;
@@ -46,10 +46,11 @@ export const login = async ( req, res) => {
         if (!user ) return res.status(400).json({ msg: "User does not exist. "});
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json(msg: "Invalid credentials. ");    
+        if (!isMatch) return res.status(400).json({msg: "Invalid credentials. "});    
 
         const token  = jwt.sign({ id: user._id }, process.env.JWT_SECRET);  
-        
+        delete user.password;
+        res.status(200).json({ token, user });
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
